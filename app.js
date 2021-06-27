677b0de2a5b7aa275ef39fd25aa994fa837f79b5
John Lanni <Johnlanni619@gmail.com>
2021-06-27 16:47:29 -0400

    Added inquire functionality and write to html

--- app.js
+++ app.js
@@ -10,10 +10,152 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+let teamDataArray = [];
 
+const addTeamMember = teamData => {
+    return inquirer
+        .prompt([
+        {
+            name: 'employeeType',
+            message: 'Which type of employee would you like to add?',
+            type: 'rawlist',
+            choices: ['Engineer', 'Intern', 'Manager']
+        }
+        ])
+        .then(response => {
+            if (response.employeeType === 'Engineer') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'engineerName',
+                        message: "Enter your Engineer's name:",
+                    },
+                    {
+                        type: 'input',
+                        name: 'engineerId',
+                        message: "Enter your Engineer's ID:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'engineerEmail',
+                        message: "Enter your Engineer's email address:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'engineerGithub',
+                        message: "Enter your Engineer's GitHub username:"
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'confirmAddTeamMember',
+                        message: 'Would you like to add another employee?'
+                    },
+                ])
+                .then(response => {
+                    let newEngineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub)
+
+                    teamDataArray.push(newEngineer);
+
+                    if (response.confirmAddTeamMember) {
+                        addTeamMember();
+                    }
+                    else {
+                        writeToHTML();
+                    }
+                })
+            }
+            if (response.employeeType === 'Intern') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'internName',
+                        message: "Enter your Intern's name:",
+                    },
+                    {
+                        type: 'input',
+                        name: 'internId',
+                        message: "Enter your Intern's ID:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'internEmail',
+                        message: "Enter your Intern's email address:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'internSchool',
+                        message: "Enter your Intern's school name:"
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'confirmAddTeamMember',
+                        message: 'Would you like to add another employee?'
+                    },
+                ])
+                .then(response => {
+                    let newIntern = new Intern(response.internName, response.internId, response.internEmail,response.internSchool)
+
+                    teamDataArray.push(newIntern);  
+
+                    if (response.confirmAddTeamMember) {
+                        addTeamMember();
+                    }
+                    else {
+                        writeToHTML();
+                    }
+                })
+            }
+            if (response.employeeType === 'Manager') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'managerName',
+                        message: "Enter your Manager's name:",
+                    },
+                    {
+                        type: 'input',
+                        name: 'managerId',
+                        message: "Enter your Manager's ID:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'managerEmail',
+                        message: "Enter your Manager's email address:"
+                    },
+                    {
+                        type: 'input',
+                        name: 'managerOfficeNumber',
+                        message: "Enter your Manager's office number:"
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'confirmAddTeamMember',
+                        message: 'Would you like to add another employee?'
+                    },
+                ])
+                .then(response => {
+                    let newManager = new Manager(response.managerName, response.managerId, response.managerEmail,response.managerOfficeNumber)
+
+                    teamDataArray.push(newManager);  
+
+                    if (response.confirmAddTeamMember) {
+                        addTeamMember();
+                    }
+                    else {
+                        writeToHTML();
+                    }
+                })
+            }
+        })
+}
+
+addTeamMember();
+
+function writeToHTML() {
+    fs.writeFile("team.html", render(teamDataArray), err => err ? console.log(err) : console.log('Heres your new file!'))
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
